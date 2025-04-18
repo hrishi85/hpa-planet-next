28671835bffbc5bb0b993bc297f47125b1051790
Hrishikesh Yashwant Joshi <hrishi@Hrishikeshs-iMac-2.local>
2025-04-18 09:50:52 +0530

    changed page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,15 @@
+import { Image } from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+	const [emailText, setEmailText] = useState("");
+	const [passwordText, setPasswordText] = useState("");
+
+	const handleEmailChange =  () => {
+		console.log("test");
+	};
+
 	return (
-		<h1>Test</h1>
+		<h1 onChange={handleEmailChange}>Test</h1>
 	);
 }
