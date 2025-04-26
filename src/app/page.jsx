808e53466de0938f0d8b3bf8b91418996c158c30
Hrishikesh Yashwant Joshi <hrishi@Hrishikeshs-iMac-2.local>
2025-04-26 10:38:00 +0530

    added newsp data fetching in src/app/page.tsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,17 +12,17 @@ const baseURL = process.env.STRAPI_API_URL || 'http://localhost:1337';
 
 async function fetchData() {
 	try {
-		const [tracksRes, servicesRes] = await Promise.all([
+		const [tracksRes, servicesRes, newspRes] = await Promise.all([
 			fetch(`${baseURL}/api/tracks?populate=banner&pagination[limit]=5`).then(res => res.json()),
 			fetch(`${baseURL}/api/services?populate=thumbnail`).then(res => res.json()),
-			// fetch(`${baseURL}/api/newsp?pagination[limit]=4&populate=banner`).then(res => res.json()),
+			fetch(`${baseURL}/api/newsp?pagination[limit]=4&populate=banner`).then(res => res.json()),
 			// fetch(`${baseURL}/api/history`).then(res => res.json()),
 		]);
   
 		return {
 			tracks: tracksRes.data || [],
 			services: servicesRes.data || [],
-			// newsp: newspRes.data || [],
+			newsp: newspRes.data || [],
 			// history: historyRes.data || null,
 			error: null,
 		};
@@ -31,7 +31,7 @@ async function fetchData() {
 		return {
 			tracks: [],
 			services: [],
-			// newsp: [],
+			newsp: [],
 			// history: null,
 			error: 'Failed to load content. Please try again later.',
 	  	};
@@ -41,7 +41,7 @@ async function fetchData() {
 
 export default async function Home() {
 
-	const { tracks, services, error } = await fetchData();
+	const { tracks, services, newsp, error } = await fetchData();
 
 	if (error) {
 		return <div className="flex w-full h-full items-center justify-center text-red-600">{error}</div>;
@@ -58,10 +58,10 @@ export default async function Home() {
 			}
 			{/* {
 				history && history.companyHistory && ( <History history={history.companyHistory} /> )
-			}
+			} */}
 			{
 				newsp && newsp.length > 0 && ( <News news={newsp} /> )
-			} */}
+			}
 			<Contact />
 			<PagePFooter />
 			<PageFooter />
